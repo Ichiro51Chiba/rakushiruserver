1312fc58637e2e3096eacbd868e253d4ab359a3f
Ichir51Chiba <chibaichiro@icloud.com>
2020-07-11 17:44:52 +0900

    view閲覧制限機能追加

--- api_zaikodata/views.py
+++ api_zaikodata/views.py
@@ -3,6 +3,8 @@ from .models import ZaikoData
 from django.contrib.auth.models import User
 from .serializers import ZaikoDataSerializers,UserSerializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny
 
 class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
@@ -11,3 +13,5 @@ class UserViewSet(viewsets.ModelViewSet):
 class ZaikoDataViewSet(viewsets.ModelViewSet):
   queryset = ZaikoData.objects.all()
   serializer_class = ZaikoDataSerializers
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (IsAuthenticated,)
