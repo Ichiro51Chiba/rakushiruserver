0a253ac68fb8b638718fc2950048dff464769e76
Ichir51Chiba <chibaichiro@icloud.com>
2020-07-11 09:02:19 +0900

    token追加

--- api_zaikodata/serializers.py
+++ api_zaikodata/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from .models import ZaikoData
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 class UserSerializers(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id','username','password')
+    fields = ('id','username','password','email')
     extra_kwargs = {'password':{'write_only':True,'required':True}}
   
   def create(self,validated_data):
     user = User.objects.create_user(**validated_data)
+    Token.objects.create(user=user)
     return user
   
 class ZaikoDataSerializers(serializers.ModelSerializer):
