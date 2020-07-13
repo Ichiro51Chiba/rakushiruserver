6eedb78daf73e8ef76e39680a57435e3b8893402
Ichir51Chiba <chibaichiro@icloud.com>
2020-07-13 19:20:55 +0900

    データ反映

--- api_zaikodata/models.py
+++ api_zaikodata/models.py
@@ -10,18 +10,28 @@ class Zaiko(models.Model):
     ('2','中古'),
   )
   
-
+  # 勘定コード
   kanzyo = models.IntegerField(validators=  [MinValueValidator(0)])
   # model_no = models.AutoField
+  # 規格コード
   code = models.IntegerField(validators=  [MinValueValidator(0)])
+  # 評価単価
   price = models.IntegerField(validators=  [MinValueValidator(0)])
+  # 新品/中古
   status = models.CharField(max_length=40,choices=STATUS,  default=1)
+  # 実残数
   number = models.IntegerField(validators=  [MinValueValidator(0)])
+  # 品目名称
   name = models.CharField(max_length=50)
+  # 合計金額
   total_price = models.IntegerField(validators=  [MinValueValidator(0)])
+  # 備考
   remark = models.CharField(max_length=70)
+  # 写真
   pic = models.ImageField(upload_to=None,   height_field=None, width_field=None, max_length=100)
+  # 作成日
   create_at = models.DateTimeField(auto_now_add=True)
+  # 更新日
   updated_at = models.DateTimeField(null=True,blank=True)
   
   def __str__(self):
